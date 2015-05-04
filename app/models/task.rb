feef9102bfa6ef38c3b07c931ec3c3a9ea34db88
Alexander Kovryga <sashakovryga@gmail.com>
2015-05-05 02:48:38 +0300

    add tasks grph

--- app/models/task.rb
+++ app/models/task.rb
@@ -10,11 +10,14 @@ class Task < ActiveRecord::Base
   enumerize :status, in: [:verify, :do, :perfomed]
 
   def time_percent(start, stop)
+    prev = Task.where('tasks.from < ?', self.from).last
+    minus = prev.nil? ? start : prev.to
     days = (60 * 60 * 24)
     total = (stop - start).to_i / days
-    befores = ( ((self.from || start) - start) / days ) / total * 100
-    afters = ( (stop - (self.to || stop)) / days ) / total * 100
-    length = 100 - befores - afters
+    befores = ( ((self.from || start) - minus) / days ) / total * 100
+    befores_prev = ( ((self.from || start) - start) / days ) / total * 100
+    afters = ( (stop - (self.to || stop)) / days ) / total  * 100
+    length = 100 - afters - (prev.nil? ? befores : befores_prev)
     {:total => total, :befores => befores, :afters => afters, :length => length}
   end
 
